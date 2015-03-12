4b89868104379647b95b3c73f561534344284dd6
yellowschedule <yellowschedule@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 11:15:11 +0000

    Submitting version 1.0.0 of yellow schedule plugin
    git-svn-id: https://plugins.svn.wordpress.org/yellow-schedule/tags/1.0.0@1111025 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * 
+ * This file runs when the plugin in uninstalled (deleted).
+ * This will not run when the plugin is deactivated.
+ * Ideally you will add all your clean-up scripts here
+ * that will clean-up unused meta, options, etc. in the database.
+ *
+ */
+
+// If plugin is not being uninstalled, exit (do nothing)
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// Do something here if plugin is being uninstalled.
+delete_option('wpt_ys_master_act');
+delete_option('wpt_ys_num_days');
+delete_option('wpt_ys_display_user');
